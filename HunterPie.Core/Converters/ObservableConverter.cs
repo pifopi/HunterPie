e9a30d342b18fe0ac615af1eb0101cc74f777273
pifopi <dottel.gael@gmail.com>
2025-04-16 14:05:31 +0200

    Handle gracefully unknown new games

--- HunterPie.Core/Converters/ObservableConverter.cs
+++ HunterPie.Core/Converters/ObservableConverter.cs
@@ -10,13 +10,23 @@ internal class ObservableConverter : JsonConverter
 
     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
     {
-
         Type T = objectType.GetGenericArguments()[0];
-        object value = T.IsEnum
-            ? Enum.Parse(T, reader.Value.ToString())
-            : reader.Value != null
-                ? Convert.ChangeType(reader.Value, T)
-                : null;
+        object value;
+        if (T.IsEnum)
+        {
+            if (Enum.TryParse(T, reader.Value.ToString(), out object enumValue))
+            {
+                value = enumValue;
+            }
+            else
+            {
+                value = null;
+            }
+        }
+        else
+        {
+            value = reader.Value != null ? Convert.ChangeType(reader.Value, T) : null;
+        }
         object observable = Activator.CreateInstance(objectType, value);
 
         if (value is null)
